4f2a93d10ede56c4fa843a72e131bef7cbe51d2a
BeSingularTeam <besingulardemo@gmail.com>
2024-05-16 23:11:42 +0530

    ReatingAndMissingNumber

---
+++ RepeatingAndMissingNumbers.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+public class tUf {
+
+    public static int[] findMissingRepeatingNumbers(int[] a) {
+        int n = a.length; // size of the array
+        int repeating = -1, missing = -1;
+
+        //Find the repeating and missing number:
+        for (int i = 1; i <= n; i++) {
+            //Count the occurrences:
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (a[j] == i) cnt++;
+            }
+
+            if (cnt == 2) repeating = i;
+            else if (cnt == 0) missing = i;
+
+            if (repeating != -1 && missing != -1)
+                break;
+        }
+        int[] ans = {repeating, missing};
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3, 1, 2, 5, 4, 6, 7, 5};
+        int[] ans = findMissingRepeatingNumbers(a);
+        System.out.println("The repeating and missing numbers are: {"+ ans[0] + ", " + ans[1] + "}");
+    }
+}
+
